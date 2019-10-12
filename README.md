512f4c3f48974828cc63b3a44e6f23d583fa978f
anca-radulian <anca.radulian@gmail.com>
2019-10-12 17:27:47 +0100

    Drawn cylinder across an axis

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Third-Year-Project
+
+Resources used:
+
+ - Matplot tutorial
+    - https://matplotlib.org/mpl_toolkits/mplot3d/tutorial.html
+    - https://jakevdp.github.io/PythonDataScienceHandbook/04.12-three-dimensional-plotting.html
+    - https://pythonprogramming.net/matplotlib-3d-scatterplot-tutorial/
+  
+  - Formulas:
+    - Euclidian Distance with numpy: https://stackoverflow.com/questions/1401712/how-can-the-euclidean-distance-be-calculated-with-numpy
+    - 3D Circle equation: https://math.stackexchange.com/questions/1184038/what-is-the-equation-of-a-general-circle-in-3-d-space
+    - Plane perpedicular to a line : https://math.stackexchange.com/questions/987488/equation-for-a-plane-perpendicular-to-a-line-through-two-given-points
+    - 3D circle equation with numpy: https://stackoverflow.com/questions/48335279/given-general-3d-plane-equation-how-can-i-plot-this-in-python-matplotlib
+    - equation of a line 3D: https://brilliant.org/wiki/3d-coordinate-geometry-equation-of-a-line/
+    - Equations of lines and planes: https://www.math.tamu.edu/~glahodny/Math251/Section%2011.4.pdf
+    - Sphere and plane intersection: http://www.ambrsoft.com/TrigoCalc/Sphere/SpherePlaneIntersection_.htm
+    - 3D equation of a plane: https://brilliant.org/wiki/3d-coordinate-geometry-equation-of-a-plane/
+    
+    These two were used to write the code
+    http://mathforum.org/library/drmath/view/51734.html
+    https://stackoverflow.com/questions/32317247/how-to-draw-a-cylinder-using-matplotlib-along-length-of-point-x1-y1-and-x2-y2?rq=1
+    
--- grapeStemGenerator.py
+++ grapeStemGenerator.py
@@ -1,1 +1,73 @@
-print("Hello")
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlim(0, 30)
+ax.set_ylim(0, 30)
+ax.set_zlim(0, 30)
+
+# Coordinates chosen
+user_x = 20
+user_y = 10
+user_z = 30
+
+c = np.array([user_x, user_y, user_z])
+o = np.array([10, 15, 10])
+
+# plot the axis of the
+ax.plot(*zip(c, o), color = 'red')
+
+# radius of the circle
+R = 3
+
+#  ==== Plotting the perpendicular plane =====
+# formula to calculate the plane equation
+# a(x−x0)+b(y−y0)+c(z−z0)=0
+x = np.linspace(0, 40, 10)
+y = np.linspace(0, 40, 10)
+X,Y = np.meshgrid(x, y)
+Z = (-user_x *( X - user_x) - user_y * (Y - user_y)) / user_z + user_z
+
+# ax.plot_surface(X, Y, Z)
+
+# =========================
+
+# Equation of the circle
+#    (x,y,z) = (x0, y0, z0) + cos(theta)u + sin(theta)v + t*w
+# where theta varies over the interval 0 to 2pi and t ranges over the
+# the set of real numbers and
+# u and v are unit vectors that are (1) mutually perpendicular and (2) are perpendicular
+# to the axis , w = unit vector of the chosen axis
+
+# vector between the two points
+w = c - o
+# mag of vector
+mag = np.linalg.norm(w)
+
+# unit vector
+w = w / mag
+
+# make some vector not in the same direction as v
+not_w = np.array([1, 0, 0])
+if (w == not_w).all():
+    not_w = np.array([0, 1, 0])
+
+# make vector perpendicular to v
+u = np.cross(w, not_w)
+# normalize n1
+u /= np.linalg.norm(u)
+# make unit vector perpendicular to v and n1
+v = np.cross(w, u)
+
+# surface ranges over t from 0 to length of axis and 0 to 2*pi
+t = np.linspace(0, mag, 100)
+theta = np.linspace(0, 2 * np.pi, 100)
+# use meshgrid to make 2d arrays
+t, theta = np.meshgrid(t, theta)
+# generate coordinates for surface
+X, Y, Z = [o[i] + w[i] * t + R * np.sin(theta) * v[i] + R * np.cos(theta) * u[i] for i in [0, 1, 2]]
+ax.plot_surface(X, Y, Z, color = 'purple')
+
+plt.show()
